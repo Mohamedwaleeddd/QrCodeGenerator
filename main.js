8dfcd66daf6d5d32620b7eff81fbed8cd54da227
Mohamedwaleeddd <122593986+Mohamedwaleeddd@users.noreply.github.com>
2023-10-06 06:49:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let qrText = document.getElementById("qrText");
+let qrImage = document.getElementById("qrImage");
+let box = document.querySelector('.message');
+
+box.style.display = "none";
+
+function remove(){
+  box.style.left = '-100%';
+  box.style.display = 'none';
+}
+
+function generateQR(){
+  if(qrText.value.length > 0){
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example"+ qrText.value;
+  }else{
+    box.style.display = 'grid';
+    box.style.left = '50%';
+  }
+}
